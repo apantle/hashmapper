ddc4acc3337eec8d26e00a5d3dc4bb840182e0c5
Jesus E. Franco Martinez <jefrancomix@gmail.com>
2019-11-25 18:04:03 -0600

    Preparing tests for higher PHPUnit versions

--- tests/ThrowLogicExceptionIfNotCallableTest.php
+++ tests/ThrowLogicExceptionIfNotCallableTest.php
@@ -4,13 +4,11 @@ use Apantle\HashMapper\HashmapMapper;
 
 class ThrowLogicExceptionIfNotCallableTest extends PHPUnit\Framework\TestCase
 {
-
-    /**
-     * @expectedException \LogicException
-     * @expectedExceptionMessage mapper is not a callable or instance of HashmapMapperInterface
-     */
     public function testApply()
     {
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('mapper is not a callable or instance of HashmapMapperInterface');
+
         $hm = new HashmapMapper([
             'input' => ['target', 'not_a_callable']
         ]);
